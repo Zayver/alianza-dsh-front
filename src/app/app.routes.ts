5463c9dd28ba4f8cd83a43b63debb33cce0a95f3
Kevyn Prada <koprada@javeriana.edu.co>
2024-12-13 16:44:35 -0500

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', loadComponent: ()=> import('@components/shared/header/header.component').then(m => m.HeaderComponent) },
+    {path: '', loadComponent: ()=> import('@components/shared/main/main.component').then(m => m.MainComponent)},
     {path: 'about-us', loadComponent: ()=> import('@components/shared/about-us/about-us.component').then((m) => m.AboutUsComponent) },
 ];
