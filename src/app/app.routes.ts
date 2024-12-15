ecd8acb20d9287b9eb8e53d55ef30b8660cca955
Kevyn Prada <koprada@javeriana.edu.co>
2024-12-15 09:57:24 -0500

    home created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,8 @@ export const routes: Routes = [
         path: '', loadComponent: () => import('@components/shared/main/main.component').then(m => m.MainComponent),
         title: 'Inicio | Alianza DSH',
         children: [
-            { path: 'contact', title: 'Contáctanos | Alianza DSH', loadComponent: () => import('@components/contactus/contactus.component').then(m => m.ContactusComponent)},
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'contact', title: 'Contáctanos | Alianza DSH', loadComponent: () => import('@components/contactus/contactus.component').then(m => m.ContactusComponent) },
             { path: 'products', title: 'Productos | Alianza DSH', loadComponent: () => import('@components/products/products.component').then(m => m.ProductsComponent) },
             { path: 'about-us', title: 'Quienes somos | Alianza DSH', loadComponent: ()=> import('@components/aboutus/aboutus.component').then((m) => m.AboutusComponent) },
             { path: 'home', title: 'Inicio | Alianza DSH', loadComponent: ()=> import('@components/home/home.component').then(m => m.HomeComponent) },
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { lucideBoxes, lucideLayers, lucideCable } from '@ng-icons/lucide';
 
 @Component({
-  selector: 'alianzadsh-home',
-  imports: [],
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  imports: [NgIconComponent, CommonModule], 
+  providers: [provideIcons({ lucideBoxes, lucideLayers, lucideCable })],
 })
 export class HomeComponent {
-
+  team = [
+    {
+      name: 'Juan Pérez',
+      role: 'Director General',
+      contact: 'juan.perez@email.com',
+      imageUrl: 'images/home/profile_home.jpg',
+    },
+    {
+      name: 'María López',
+      role: 'Gerente de Proyectos',
+      contact: 'maria.lopez@email.com',
+      imageUrl: 'images/home/profile_home2.jpg',
+    },
+    {
+      name: 'Carlos Gómez',
+      role: 'Coordinador Técnico',
+      contact: 'carlos.gomez@email.com',
+      imageUrl: 'images/home/profile_home3.jpg',
+    },
+  ];
 }
