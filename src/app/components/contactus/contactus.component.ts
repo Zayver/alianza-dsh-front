5cadcf9cc6a3453bcc7f35c242a586738731078f
Kevyn Prada <koprada@javeriana.edu.co>
2024-12-20 09:45:21 -0500

    some changes

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,52 +1,93 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Button } from 'primeng/button'
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
-import { InputText } from 'primeng/inputtext'
-import { InputNumber } from 'primeng/inputnumber'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputText } from 'primeng/inputtext';
+import { InputNumber } from 'primeng/inputnumber';
 import { KeyFilter } from 'primeng/keyfilter';
-import { Textarea } from 'primeng/textarea'
-import { Select } from 'primeng/select'
+import { Textarea } from 'primeng/textarea';
+import { Select } from 'primeng/select';
+import { FileUpload } from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
 import { finalize, of } from 'rxjs';
 
 @Component({
   selector: 'alianzadsh-contactus',
-  imports: [ ReactiveFormsModule, InputText, InputNumber, Textarea, Select, KeyFilter, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    InputText,
+    InputNumber,
+    Textarea,
+    Select,
+    KeyFilter,
+    CommonModule,
+    FileUpload
+  ],
   templateUrl: './contactus.component.html',
-  styleUrl: './contactus.component.scss'
+  styleUrls: ['./contactus.component.scss'],
 })
-export class ContactusComponent implements OnInit{
-  private readonly formBuilder = inject(FormBuilder)
-  contactForm!: FormGroup;
-  loading = signal(false)
+export class ContactusComponent implements OnInit {
+  private readonly formBuilder = inject(FormBuilder);
+  form!: FormGroup;
+  loading = signal(false);
 
   readonly services = [
-    {value: 'SOFTWARE', display: 'Software'},
-    {value: 'HARDWARE', display: 'Hardware'},
-    {value: 'SUPPORT_SERVICE', display: 'Soporte'}
-  ]
+    { value: 'SOFTWARE O HARDWARE', display: 'Software o Hardware' },
+    { value: 'SUPPORT_SERVICE', display: 'Soporte' },
+  ];
+
+  selectedFiles: File[] = [];
 
   ngOnInit(): void {
-    this.contactForm = this.formBuilder.group({
+    // Inicialización del formulario
+    this.form = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       companyName: ['', Validators.required],
-      employeeQuantity: [null],
+      employeeQuantity: [{ value: null, disabled: false }],
       email: ['', [Validators.required, Validators.email]],
       tel: ['', Validators.required],
       comments: [''],
-      serviceType: ['', Validators.required]
-    })
+      serviceType: ['', Validators.required],
+      attachments: [null],
+    });
+
+    // Suscripción al cambio de valor en `serviceType`
+    this.form.get('serviceType')?.valueChanges.subscribe((value) => {
+      if (value === 'SUPPORT_SERVICE') {
+        this.form.get('employeeQuantity')?.disable(); // Deshabilitar si es "Soporte"
+      } else {
+        this.form.get('employeeQuantity')?.enable(); // Habilitar en otros casos
+      }
+    });
   }
 
-  sendContactForm(){
-    const request = this.contactForm.value
-    //TODO make network request to backend
-    this.loading.set(true)
-    of({}).pipe(finalize(()=> this.loading.set(false))).subscribe({
-      next: ()=> {
-        console.log(request)
-      }
-    })
+  sendContactForm() {
+    const request = this.form.value;
+    request.attachments = this.selectedFiles;
+
+    // Simulación de envío
+    this.loading.set(true);
+    of({})
+      .pipe(finalize(() => this.loading.set(false)))
+      .subscribe({
+        next: () => {
+          console.log('Formulario enviado:', request);
+          this.form.reset(); // Limpiar el formulario después del envío
+          this.selectedFiles = []; // Limpiar archivos seleccionados
+        },
+        error: (err) => console.error('Error al enviar formulario:', err),
+      });
+  }
+
+  onFileUpload(event: any): void {
+    const files = event.files;
+    this.selectedFiles = files; // Almacenar archivos cargados
+    console.log('Archivos cargados:', files);
   }
 }
